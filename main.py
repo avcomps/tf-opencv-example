ec21de155991f8dfff22a0c297546fd80dcdf14e
Ali <aasaidi54@gmail.com>
2022-02-15 21:04:58 +0100

    Initial commit on dev branch

--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 
 #!/usr/bin/env python3
 
+from typing import Any
 from PIL import Image
 
 def main() :
-    pass    
+    img_pixels = Any; img_size = ()
+    colors_repeated = []
+
+    with Image.open("example_1.jpg") as img: img_pixels = img.load(); img_size = img.size
+    
+    print(img_size)
+    print(img_pixels[4, 4])
+
 
 if __name__ == "__main__":
     try :
