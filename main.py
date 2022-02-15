a042f03da9cc371893d689697d7632209ebb569d
Ali <aasaidi54@gmail.com>
2022-02-15 20:58:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# MIT License @ Ali Vorobiev (https://github.com/avcomps).
+
+#!/usr/bin/env python3
+
+from PIL import Image
+
+def main() :
+    pass    
+
+if __name__ == "__main__":
+    try :
+        main()
+    except(KeyboardInterrupt) :
+        print("\nERROR: KeyboardInterrupt")
