01d8e3b075d61ce6e89dc52d7ac984107a90993d
Ali <aasaidi54@gmail.com>
2022-02-15 22:17:06 +0100

    Minor commit

--- main.py
+++ main.py
@@ -2,18 +2,24 @@
 
 #!/usr/bin/env python3
 
-from typing import Any
-from PIL import Image
+from PIL import Image, ImageColor
 
 def main() :
-    img_pixels = Any; img_size = ()
-    colors_repeated = []
+    img_size = (); img_pixels = None
+    list_colors = []
 
     with Image.open("example_1.jpg") as img: img_pixels = img.load(); img_size = img.size
     
-    print(img_size)
-    print(img_pixels[4, 4])
+    max_size_x = img_size[0] - 1
+    max_size_y = img_size[1] - 1
 
+    for x in range(max_size_x) :
+        for y in range(max_size_y) :
+            if(img_pixels[x, y]) not in list_colors :
+                # list_colors.append(ImageColor.getcolor(img_pixels[x, y], 'RGB'))
+                pass
+    
+    print(list_colors)
 
 if __name__ == "__main__":
     try :
